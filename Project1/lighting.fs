02bdf5708f2049d5e7a247a75a2bbefc17b16e73
Binh2 <hgqbinh2002@gmail.com>
2021-07-09 10:57:14 +0700

    Added material and light struct to keep material and light properties

--- Project1/lighting.fs
+++ Project1/lighting.fs
@@ -1,4 +1,18 @@
 #version 330 core
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+}; 
+struct Light {
+    vec3 position;
+  
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
 in vec3 normal;
 in vec3 fragPos;
 
@@ -6,25 +20,25 @@ out vec4 fragColor;
 
 uniform vec3 objectColor;
 uniform vec3 lightColor;
-uniform vec3 lightPos;
 uniform vec3 viewPos;
-//uniform vec3 frontView;
+
+uniform Material material;
+
+uniform Light light;
 
 void main()
 {
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
+    vec3 ambient = material.ambient * light.ambient;
 
     vec3 norm = normalize(normal);
-    vec3 lightDir = normalize(lightPos - fragPos);
+    vec3 lightDir = normalize(light.position - fragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
+    vec3 diffuse = (diff * material.diffuse) * light.diffuse;
 
-    float specularStrength = 0.75;
     vec3 viewDir = normalize(viewPos - fragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;
+    vec3 specular = (material.specular * spec) * light.specular;
 
     vec3 result = (ambient + diffuse + specular) * objectColor;
     fragColor = vec4(result, 1.0);
